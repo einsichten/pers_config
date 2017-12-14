eaa835df320f0c9c55b92ef70cdb943f5e3a5f3b
Stephan Stamm <stephan.stamm@gmail.com>
2017-12-14 09:21:54 +0100

    Minor changes && cleaning

--- .home.zsh
+++ .home.zsh
@@ -1,23 +1,39 @@
 #!/usr/bin/zsh
 # Should be sourced in ~/.zshrc after `source $ZSH/oh-my-zsh.sh`
 # to override aliases and keybindings. Don't source this file in
-# ~/.zprofile, because ~/.zprofile is sourced before ~/.zshrc.
+# ~/.zprofile, which is sourced before ~/.zshrc.
 
-bindkey '^_' backward-delete-word # ctrl-backspace
+bindkey '^_' backward-delete-word    # ctrl-backspace
+bindkey '^h' backward-delete-word
 
 function f () {
   findResults=$(find -iname "$1")
   echo "$findResults" | nl
-  findResults=$(echo "$findResults" | xargs realpath)
+  if [ -n "$findResults" ]; then
+    findResults=$(echo "$findResults" | xargs realpath)
+  else
+    echo \""$1"\" not found.
+  fi
 }
 
-function fn () {
-  filepath=$(echo "$findResults" | sed "$1q;d")
+function fn () {            # findresult the nth
+  if [ -z "$1" ]; then
+    pos=1                   # defaults to first
+  else
+    pos=$1
+  fi
+  filepath=$(echo "$findResults" | sed "${pos}q;d")
   echo $filepath
 }
 
-function fnd () {
+function fpd () { # filepath directory
   cygpath -aw $(dirname $filepath)
+}
+
+function fpo() { #filepath open
+  local dir=$(dirname "$filepath")
+  local file=$(basename "$filepath")
+  (cd "$dir"; open "$file")
 }
 
 function svlog () {
@@ -33,21 +49,19 @@ alias h='history|tail -n 20'
 alias l=ls
 
 alias ag='alias|grep'
-alias diff=colordiff
+alias diff='diff --color'
 alias dirs='dirs -v'
 alias gcam='git add . && git commit -m'
 alias glog='git log --oneline'
 alias grep='grep -iP'
 alias gsta='git stash apply'
-unalias gstaa
-unalias gstc  # git stash clear too dangerous
+unalias gstaa 2>/dev/null # use 'gsta' instead
+unalias gstc  2>/dev/null # git stash clear too dangerous
 alias gsts='git stash save'
 alias gstll='git stash list --date=local'
 alias gstls='git stash list --date=short'
 alias hg='history|grep'
 alias fcsp='find -name "*.csproj" 2>/dev/null'
-alias fno='open $filepath'
-alias flog='svlog $filepath'
 alias la='ls -a'
 alias open=cygstart
 alias pi='ssh $pi'
@@ -59,13 +73,12 @@ alias upda='cp $HOME/pers_config/.h* ~; source ~/.home.zsh  # update alias'
 alias vim=vi
 alias wpwd='cygpath -w $(pwd) > /dev/clipboard && echo clipboard contains: $(cat /dev/clipboard)'
 
-alias -g 2c='> /dev/clipboard'
-alias -g ne='2>/dev/null'
+alias -g 2c='> /dev/clipboard'  # to clipboard
+alias -g ne='2>/dev/null'       # no error
 alias -s pl=perl
 alias -s sh=bash
 alias -s zsh=zsh
 
-today=$(date +%Y-%m-%d)
 doc="$HOME/Dokumente"
 c=/cygdrive/c
 export gitignore_folder="$HOME/.gitignore-files"
--- .host.DEAGM02828.zsh
+++ .host.DEAGM02828.zsh
@@ -1,21 +1,18 @@
-local todaysLogin=~/.logins/$today
+export http_proxy="http://fra4.sme.zscalertwo.net:10415"
+export https_proxy=$http_proxy
+
+local todaysLogin="$HOME/.logins/$(date +%Y-%m-%d)"
 if [[ ! -e $todaysLogin ]] then
     touch $todaysLogin
     cd $doc
-    echo "Last commit:"
-    git log -1 --format=%cd
-    if [[ $HOSTNAME == 'DEAGM02828' ]] then
-        cygstart "$doc/Zeiterfassung.xlsx"
-    fi
+    echo "Last commit: $(git log -1 --format=%cd)"
+    cygstart "$doc/Zeiterfassung.xlsx"
 fi
 
-refa="$c/projects_svn/MachineSafeguardingSuite/trunk"
-alias m1s='open /cygdrive/c/projects_svn/MachineSafeguardingSuite/trunk/MSS.Module1/src/MSS.Module1.sln' # solution module1
-alias zeit='cygstart $doc/Zeiterfassung.xlsx'
-alias logout='backup_util && cd $doc && git add . && git commit -m $today && git push'
 alias backup_util='cp -urfv $c/Util ~/Dokumente/'
-u=/cygdrive/u
-desktop=$c/Users/xsostas/Desktop
+alias logout='backup_util && cd $doc && git add . && git commit -m $today && git push'
+alias zeit='cygstart $doc/Zeiterfassung.xlsx'
 
-export http_proxy="http://fra4.sme.zscalertwo.net:10415"
-export https_proxy=$http_proxy
+desktop=$c/Users/xsostas/Desktop
+refa="$c/projects_svn/MachineSafeguardingSuite/trunk"
+u=/cygdrive/u
